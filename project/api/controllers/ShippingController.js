2f0cce680aff452c3d888e8fda3d22d0e2edee70
Peter Yu <pyu@atlassian.com>
2016-03-29 13:56:43 +1100

    Renamed project directory and generated new shipping api template

---
+++ project/api/controllers/ShippingController.js
@@ -0,0 +1,11 @@
+/**
+ * ShippingController
+ *
+ * @description :: Server-side logic for managing shippings
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
