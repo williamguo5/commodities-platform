14edd96a9e07a44cf3037d40236326d252c1b030
Peter Yu <pyu@atlassian.com>
2016-03-31 21:15:14 +1100

    Merged remote branch with new files into local branch

--- project/api/models/Shipping.js
+++ project/api/models/Shipping.js
@@ -8,7 +8,39 @@
 module.exports = {
 
   attributes: {
+    grain: {
+      type: 'string',
+      defaultsTo: '',
+    },
+    
+    date: { 
+      type:'date',
+      defaultsTo: '',
+
+    },
+    
+    portY1: {
+      collection: 'port',
+      defaultsTo: [],
+      via: 'ownerPortY1'
+    },
+    
+    portY2: {
+      collection: 'port',
+      defaultsTo: [],
+      via: 'ownerPortY2'
+    },
+    
+    avgY1: { 
+      type: 'float',
+      defaultsTo: 0.00,
+    },
+    
+    avgY2: { 
+      type: 'float',
+      defaultsTo: 0.00,
+    }
+
 
   }
 };
-
