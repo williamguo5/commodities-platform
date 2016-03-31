227ce9a7ced7015c2749be976db6aea67e24c118
Albert Kerr <ankerr305@gmail.com>
2016-03-31 17:26:43 +1100

    oops forgot Port model

---
+++ project/api/models/Port.js
@@ -0,0 +1,21 @@
+/**
+ * Port.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    name : { type: 'string' },
+
+    price : { type: 'float' },
+
+    ownerPortY1 : { model: 'shipping' },
+
+    ownerPortY2 : { model: 'shipping' }
+
+  }
+};
+
