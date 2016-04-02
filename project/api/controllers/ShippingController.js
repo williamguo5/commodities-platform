25730afc0953c5bd4e756041f74d366ff007e031
Peter Yu <pyu@atlassian.com>
2016-04-02 12:39:41 +1100

    Added createRecord endpoint to shipping controller

--- project/api/controllers/ShippingController.js
+++ project/api/controllers/ShippingController.js
@@ -6,14 +6,25 @@
  */
 
 module.exports = {
-  prices: function(req, res) {
-    var grain = req.query.grain;
-    var startDate = req.query.startDate;
-    var endDate = req.query.endDate;
-    ShippingService.prices(grain, startDate, endDate, function(prices){
-      res.json(prices);
-    });
-  }
+    getPrices: function(req, res) {
+        var grain = req.query.grain;
+        var startDate = req.query.startDate;
+        var endDate = req.query.endDate;
+        ShippingService.getPrices(grain, startDate, endDate, function(getPrices){
+            res.json(getPrices);
+        });
+    }
+
+    createRecord: function(req, res) {
+        var port = req.query.port;
+        var grain = req.query.grain;
+        var year = req.query.year;
+        var date = req.query.date;
+        var price = req.query.price;
+        ShippingService.createRecord(port, grain, year, date, price, function(createRecord) {
+            res.json(createRecord);
+        });
+    }
 
   // addRecord: function(req, res) {
   //   var grain = req.get('grain');
