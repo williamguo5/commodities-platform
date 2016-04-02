c7a86352426663478d9f287a645fb0fd788058fc
Peter Yu <pyu@atlassian.com>
2016-04-02 13:20:41 +1100

    Changed endpoint names and added createRecord function to shipping service

--- project/api/controllers/ShippingController.js
+++ project/api/controllers/ShippingController.js
@@ -10,10 +10,11 @@ module.exports = {
         var grain = req.query.grain;
         var startDate = req.query.startDate;
         var endDate = req.query.endDate;
-        ShippingService.getPrices(grain, startDate, endDate, function(getPrices){
-            res.json(getPrices);
+
+        ShippingService.getPrices(grain, startDate, endDate, function(prices) {
+            res.json(prices);
         });
-    }
+    },
 
     createRecord: function(req, res) {
         var port = req.query.port;
@@ -21,21 +22,10 @@ module.exports = {
         var year = req.query.year;
         var date = req.query.date;
         var price = req.query.price;
-        ShippingService.createRecord(port, grain, year, date, price, function(createRecord) {
-            res.json(createRecord);
+
+        ShippingService.createRecord(port, grain, year, date, price, function(success) {
+            res.json(success);
         });
     }
-
-  // addRecord: function(req, res) {
-  //   var grain = req.get('grain');
-  //   var date = req.get('date');
-  //   ShippingService.addRecord(grain, date, function(success) {
-  //     res.json(success);
-  //   });
-  // }
-
-  // addPort: function(req, res) {
-
-  // }
 };
 
--- project/api/services/ShippingService.js
+++ project/api/services/ShippingService.js
@@ -1,16 +1,21 @@
 module.exports = {
-  prices: function(grain, startDate, endDate, next) {
-    Shipping.find({grain: grain, date: {'>=': new Date(startDate), '<=': new Date(endDate)} })
-        .populate('portY1')
-        .populate('portY2')
-        .exec(function(err,prices) {
-        if (err) throw err;
 
-        // for (price in prices) {
-          // console.log(price); 
-        // }
+	getPrices: function(grain, startDate, endDate, next) {
+		Shipping.find({grain: grain, date: {'>=': new Date(startDate), '<=': new Date(endDate)} })
+	        .populate('portY1')
+	        .populate('portY2')
+	        .exec(function(err, prices) {
+		        if (err) throw err;
+		    	next(prices);
+	    	}
+	    );
+	}
 
-        next(prices);
-    });
-  }
+	createRecord: function(port, grain, year, date, price, next) {
+		Shiping.create({})
+			.exec(function(err, record)) {
+				if (err) throw err;
+				next(record);
+			}
+	}
 };
