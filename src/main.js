8badf8a787ccf9ed0e1636f420d9d531ea044240
Sam Wemyss <samuel.wemyss@gmail.com>
2016-04-05 16:13:25 +1000

    fixed 404 on route refresh

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import 'styles/main';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, Link, browserHistory } from 'react-router';
+import { Router, Route, IndexRoute, Link, hashHistory } from 'react-router';
 import Home from 'pages/home';
 import Api from 'pages/api';
 import Footer from 'components/footer/footer';
 
 
 ReactDOM.render((
-  <Router onUpdate={() => window.scrollTo(0, 0)} history={browserHistory}>
+  <Router onUpdate={() => window.scrollTo(0, 0)} history={hashHistory}>
     <Route path="/" component={Footer}>
       <IndexRoute component={Home}/>
       <Route path="api" component={Api}/>
