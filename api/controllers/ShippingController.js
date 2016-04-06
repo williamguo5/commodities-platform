b39947d6d6b2108f893b7d97f217875368166d08
Albert Kerr <ankerr305@gmail.com>
2016-04-06 22:49:43 +1000

    Copied over controller and service files from shipping_api branch

--- api/controllers/ShippingController.js
+++ api/controllers/ShippingController.js
@@ -6,34 +6,29 @@
  */
 
 module.exports = {
-	getPrices: function(req, res) {
-		var grain = req.query.grain;
-		var startDate = req.query.startDate;
-		var endDate = req.query.endDate;
+  getPrices: function(req, res) {
+    var grain = req.query.grain;
+    var startDate = req.query.startDate;
+    var endDate = req.query.endDate;
 
-		ShippingService.getPrices(grain, startDate, endDate, function(prices) {
-			res.json(prices);
-		});
-	},
+    ShippingService.getPrices(grain, startDate, endDate, function(prices) {
+      res.json(prices);
+    });
+  },
 
-	createRecords: function(req, res) {
-		// var port = req.query.port;
-		// var grain = req.query.grain;
-		// var year = req.query.year;
-		// var date = req.query.date;
-		// var price = req.query.price;
-		console.log(req.body);
-		req.file('inputData').upload(function(err, uploadedFiles) {
-			if (err) return res.send(500, err);
-
-			var SkipperDisk = require('skipper-disk');
-			var fileAdapter = SkipperDisk(/* optional opts */);
+  createRecords: function(req, res) {
+    req.file('inputData').upload(function(err, uploadedFiles) {
+      if (err) return res.send(500, err);
+      
+      var SkipperDisk = require('skipper-disk');
+      var fileAdapter = SkipperDisk(/* optional opts */);
 
             var fs = require('fs');
-			var fileContents = fs.readFileSync(uploadedFiles[0].fd, 'utf-8').toString();
+      var fileContents = fs.readFileSync(uploadedFiles[0].fd, 'utf-8').toString();
 
             fileContents = fileContents.split('\n');
-            for (var i = 0; i < fileContents.length; i++) {
+            // console.log(fileContents);
+            for (var i = 1; i < fileContents.length - 1; i++) {
                 line = fileContents[i].split(',');
 
                 var RIC = line[0].split('-');
@@ -42,18 +37,19 @@ module.exports = {
                 var year = RIC[2];
 
                 var date = line[1];
+
                 var price = line[6];
 
-                // ShippingService.createRecord(port, grain, year, date, price, function(success)) {
-                //     res.json(success);
-                // }
+                ShippingService.createRecord(port, grain, year, date, price);
+                
+                
             }
-
-			return res.json({
-				message: 'File uploaded successfully!',
-				files: uploadedFiles
-			});
-		});
-	}
+      
+      return res.json({
+        message: 'File uploaded successfully!',
+        // files: uploadedFiles.length
+      });
+    });
+  }
 };
 
--- api/services/ShippingService.js
+++ api/services/ShippingService.js
@@ -1,23 +1,90 @@
 module.exports = {
 	getPrices: function(grain, startDate, endDate, next) {
 		Shipping.find({grain: grain, date: {'>=': new Date(startDate), '<=': new Date(endDate)} })
-	        .populate('portY1')
-	        .populate('portY2')
-	        .exec(function(err, prices) {
-		        if (err) throw err;
+    	.populate('portY1')
+    	.populate('portY2')
+    	.exec(function(err, prices) {
+				if (err) throw err;
+        		
+				// sails.log.error(err);
+	        	
+				for (price in prices) {
+					// console.log(prices[price].portY1.length);
+					var sum = 0; 
+					var count = 0;
+					for (port in prices[price].portY1) {
+						// console.log(prices[price].portY1[port]);
+						var pY1 = prices[price].portY1[port];
+
+						if (pY1 != null && pY1.hasOwnProperty('price')) {
+							sum += pY1.price;
+							count++;
+						}
+							
+					}
+					if (sum == 0) {
+						prices[price].averageY1 = 0;
+					} else {
+						prices[price].averageY1 = sum/count;
+					}
+					sum = 0;
+					count = 0;
+					for (port in prices[price].portY2) {
+						var pY2 = prices[price].portY2[port];
+						if (pY2 != null && pY2.hasOwnProperty('price')) {
+							sum += pY2.price;
+							count++;
+						}
+					}
+					if (sum == 0) {
+						prices[price].averageY2 = 0;
+					} else {
+						prices[price].averageY2 = sum/count;
+					}
+
+				}
 
 				next(prices);
-	    	}
+    		}
 	    );
 	},
 
-	// createRecord: function(fileContents, next) {
-		// Shiping.create()
-			// .exec(function(err, record){
-				// if (err) throw err;
-				// next(record);
+	createRecord: function(port, grain, year, date, price) {
+		var id;
+		
+		Shipping.findOrCreate({grain: grain, date: new Date(date)})
+			.exec(function(err, record) {
+				// sails.log.error(err);
+				if (err) throw err;
+				
 
-			// }
-		// ); 		
-	// }
+				id = record.id;
+
+				var ownerPortY1;
+				var ownerPortY2;
+				if (year == "CY1") {
+					ownerPortY1 = id;
+				} else {
+					ownerPortY2 = id;
+				}
+
+				if (price == '') {
+					price = 0;
+				}
+
+				Port.create({name:port, price: price, ownerPortY1: ownerPortY1, ownerPortY2: ownerPortY2})
+					.exec(function(err, record) {
+						// sails.log.error(err);
+						if (err) throw err;
+
+						// console.log(record);
+					}
+				);
+			}
+		);
+
+		
+		
+			 		
+	}
 };
