fc70702a39634052871b1055af35c8bbf574ad24
Nethan Tran <nethan.tran@gmail.com>
2016-04-08 16:46:54 +1000

    add prototype functions in userController, track UserServices

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -15,17 +15,32 @@ module.exports = {
 		// creates new user ID then servse up to client
 		var key = UserService.randomKey();
 
-		// TODO: create new key if already taken
+		// TODO: create new key if already taken and redo
+
+		// create user
+		user.create({userID: key})
+					.exec(function(err, record) {
+						// sails.log.error(err);
+						if (err) throw err;
+						// console.log(record);
+					}
+				);
 
 		return res.json({
 			userID: key,
+			message: "Success"
 		});
 		
 	},
 	
 	clean: function(req, res) {
-		// to do
+		// TODO:
 		// removes all records user input
+	}
+
+	deleteUser: function(req, res) {
+		// TODO:
+		// removes all records for user THEN removes user
 	}
 };
 
