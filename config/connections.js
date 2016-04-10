d607a1e9185e51091636327583e6a00ef789eca9
Nethan Tran <nethan.tran@gmail.com>
2016-04-10 11:23:05 +1000

    enable heroku with mongoLab

--- config/connections.js
+++ config/connections.js
@@ -56,10 +56,12 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
+  var MONGOLAB_URI = 'mongodb://heroku_bqcs86c6:mgs96do74fkk1gctl8v6a35dal@ds019980.mlab.com:19980/heroku_bqcs86c6'
   someMongodbServer: {
     adapter: 'sails-mongo',
-    host: 'localhost',
-    port: 27017,
+    url: process.env.MONGOLAB_URI
+    // host: 'localhost',
+    // port: 27017,
     // user: 'username',
     // password: 'password',
     // database: 'your_mongo_db_name_here'
