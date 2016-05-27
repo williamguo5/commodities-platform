0dd8db830f16bb23401e533e3f56eafa8c6ee3c3
Albert Kerr <ankerr305@gmail.com>
2016-05-27 15:28:02 +1000

    Added comments to shipping.js

--- shippingAPI_v1.py
+++ shippingAPI_v1.py
@@ -18,14 +18,14 @@ def sorting(D):
 		splitup = D.split('-')
 		return splitup[0], splitup[1]
 	
-
+# Converts csv file into a 4 dimensional dictionary
 def readData(filename):
 	with open(filename) as input_file:
 		for line in list(csv.DictReader(input_file)):
 
 			RIC = line['#RIC']
 			RICfields = RIC.split('-');
-
+			# gets port, grain and year from ric code
 			port = RICfields[0]
 			grain = RICfields[1]
 			year = RICfields[2]
@@ -80,15 +80,19 @@ def findPrices(grain, startDate, endDate):
 			for port in ports:
 				sys.stdout.write(port + '_Y1":"')
 				
+				#Check if there's a Year 1 price for this particular grain and date
 				if 'CY1' in dataDict[grain][date]:
 					if port in dataDict[grain][date]['CY1']:
 						portSumCY1 = 0
 						numPricesCY1 = len(dataDict[grain][date]['CY1'][port])
+						# Sum the prices for duplicate entries of the same grain, date, port
 						for price in dataDict[grain][date]['CY1'][port]:
 							portSumCY1 += float(price)
 
+						# average this result
 						priceCY1 = portSumCY1 / numPricesCY1
 
+						# only add to the average if there price is not 0
 						if priceCY1 != 0:
 							priceSumCY1 += priceCY1
 							numPortsCY1 += 1
@@ -97,16 +101,21 @@ def findPrices(grain, startDate, endDate):
 				sys.stdout.write('","')
 				
 				sys.stdout.write(port + '_Y2":"')
-
+				
+				#Check if there's a Year 2 price for this particular grain and date
 				if 'CY2' in dataDict[grain][date]:
 					if port in dataDict[grain][date]['CY2']:
 						portSumCY2 = 0
 						numPricesCY2 = len(dataDict[grain][date]['CY2'][port])
+						
+						# Sum the prices for duplicate entries of the same grain, date, port
 						for price in dataDict[grain][date]['CY2'][port]:
 							portSumCY2 += float(price)
 
+						# average this result
 						priceCY2 = portSumCY2 / numPricesCY2
 
+						# only add to the average if there price is not 0
 						if priceCY2 != 0:
 							priceSumCY2 += priceCY2
 							numPortsCY2 += 1
@@ -118,18 +127,20 @@ def findPrices(grain, startDate, endDate):
 			# Print averages
 			
 			sys.stdout.write('average_Y1":"')
+			# Only print the average if it's not 0 and the number of the ports if not 0
 			if numPortsCY1 != 0 and priceSumCY1 != 0:
 				sys.stdout.write(str(float("{0:.2f}".format(priceSumCY1 / numPortsCY1))))
 
 			sys.stdout.write('","')
 			sys.stdout.write('average_Y2":"')
+			# Only print the average if it's not 0 and the number of the ports if not 0
 			if numPortsCY2 != 0 and priceSumCY2 != 0:
 				sys.stdout.write(str(float("{0:.2f}".format(priceSumCY2 / numPortsCY2))))
 							
 			sys.stdout.write('"}')
 			print # Print new line
 
-
+# Check if the date is in range of the start and end date
 def isInRange(startDate, endDate, dateToCompare):
 	start = datetime.strptime(startDate, "%d-%b-%Y")
 	end = datetime.strptime(endDate, "%d-%b-%Y")
@@ -138,6 +149,7 @@ def isInRange(startDate, endDate, dateToCompare):
 	if date < start or date > end: return False
 	return True
 
+# get input arguments
 fileName = sys.argv[1]
 grain = sys.argv[2]
 startDate = sys.argv[3]
